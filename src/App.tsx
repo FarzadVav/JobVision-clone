284eaceb0639404b302a6d1da5da051a3d81e183
FarzadVav <vav.progg.ir@gmail.com>
2023-09-04 04:55:02 -0700

    build project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+	return (
+		<div className={'bg-primary p-3'}>
+      div
+		</div>
+	);
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      screen: {
+        'sm': '540px'
+      },
+      colors: {
+        'jv-white': 'var(--whiteColor)',
+        'jv-black': 'var(--blackColor)',
+        'jv-primary': 'var(--primaryColor)',
+        'jv-light': 'var(--lightColor)',
+        'jv-dark': 'var(--darkColor)',
+        'jv-danger': 'var(--dangerColor)',
+        'jv-success': 'var(--successColor)',
+        'jv-warning': 'var(--warningColor)',
+        'jv-blue': 'var(--blueColor)'
+      }
+    },
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false
+  }
+}
+
