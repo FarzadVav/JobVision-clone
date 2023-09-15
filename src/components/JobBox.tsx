cedf7ae8a04b32c4ef64dffa0b771fca0eeb3a41
FarzadVav <vav.progg.ir@gmail.com>
2023-09-15 06:05:48 -0700

    add employment baner

--- src/components/JobBox.tsx
+++ src/components/JobBox.tsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom"
 import JobTypes from "../types/Job.types"
 
 const JobBox = ({ id, categories, title, company, city, location, salary, remote, isUrgent }: JobTypes) => {
-  console.log(typeof salary);
-
   return (
     <article
       className={`bg-white w-full group`}
@@ -83,7 +81,7 @@ const JobBox = ({ id, categories, title, company, city, location, salary, remote
           }
           {
             company.score ? (
-              <div className={`badge ml-auto opacity-0 group-hover:opacity-100`}>
+              <div className={`badge ml-auto lg:opacity-0 group-hover:opacity-100`}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={0} stroke="currentColor" className={`fill-jv-warning w-5 h-5 mr-0`}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                 </svg>
