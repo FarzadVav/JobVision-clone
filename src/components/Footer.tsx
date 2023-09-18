971ae735f8ac6551471f180fcb6e7fff46ee4764
FarzadVav <vav.progg.ir@gmail.com>
2023-09-18 09:10:24 -0700

    responsive footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import Title from "./Title"
 import { Link } from "react-router-dom";
+import tokenGenerator from "../utils/tokenGenerator";
 
 type FooterAccordionProps = {
   length: number;
@@ -34,7 +35,7 @@ const FooterAccordion = ({ length, title, text }: FooterAccordionProps) => {
         </span>
       </div>
       <p
-        className={`border-solid border-[#ffffff10] ${showText ? 'border-t py-3' : ''} text-white max-w-2xl
+        className={`border-solid border-[#ffffff10] ${showText ? 'border-t py-3' : ''} text-white
         cursor-text overflow-hidden`}
         ref={accordionRef}
       >
@@ -59,6 +60,19 @@ const accordions = [
   },
 ]
 
+const FooterTitle = ({ children, margin }: { children: ReactNode, margin?: boolean }) => {
+  return (
+    <Title
+      customClass={`justify-center sm:justify-start ${margin ? 'mt-6' : ''}`}
+      withOutIcon
+    >
+      <h6 className={`text-white text-center sm:text-right`}>
+        {children}
+      </h6>
+    </Title>
+  )
+}
+
 const Footer = () => {
   const [showFooterOtherContent, setShowFooterOtherContent] = useState<boolean>(false)
   const footerOtherContentRef = useRef<HTMLDivElement>(null)
@@ -75,98 +89,77 @@ const Footer = () => {
   return (
     <footer className={`bg-jv-dark z-40 py-12`}>
       <div className={`wrapper flex flex-col justify-center items-center`}>
-        <Title
-          customClass={`justify-center`}
-          withOutIcon
-        >
-          <h6 className={`text-white`}>
-            سوالات متداول درباره سایت استخدام و کاریابی جاب ویژن
-          </h6>
-        </Title>
-        <ul className={`w-max flex flex-col justify-center items-center mt-6`}>
+        <FooterTitle>
+          سوالات متداول درباره سایت استخدام و کاریابی جاب ویژن
+        </FooterTitle>
+        <ul className={`w-full flex flex-col justify-center items-center mt-6`}>
           {
             accordions.length ? accordions.map((accordion, index) => (
               <FooterAccordion
+                key={tokenGenerator()}
                 length={index + 1}
                 {...accordion}
               />
             )) : null
           }
         </ul>
-        <div className={`mt-12`}>
-          <Title
-            withOutIcon
-          >
-            <h6 className={`text-white`}>
-              استخدام با معتبرترین سایت کاریابی و استخدامی ایران
-            </h6>
-          </Title>
+        <div className={`w-full mt-12`}>
+          <FooterTitle>
+            استخدام با معتبرترین سایت کاریابی و استخدامی ایران
+          </FooterTitle>
           <p className={`text-white text-justify mt-3`}>
             پیدا کردن شغل دلخواه و فرصت استخدام جدید در سازمانی معتبر با شرایط مطلوب کاری آسان نیست. فرآیند پیدا کردن شغل و کاریابی، همواره مسیری با ناهمواری‌های زیاد برای کارجویان بوده و هست. وضعیت نه‌چندان مطلوب بازار کار در ایران از یک طرف و عدم کسب مهارت‌های تخصصی توسط کارجویان از طرف دیگر، این موضوع را به یک مسئله بزرگ در جامعه تبدیل کرده است.
 
             همه کارجویان در هر حوزه، شهر، شغل و سنی تمایل به کوتاه و آسان‌تر کردن این مسیر ناهموار و تسریع در استخدام خود دارند. برای موفقیت در این راه به یک رزومه حرفه‌ای، دسترسی به آگهی‌های استخدام جدید، شناخت سازمان‌ها، مهارت تخصصی و در نهایت ارسال رزومه به فرصت‌های شغلی متناسب با خود نیاز دارید.
+            <span className={showFooterOtherContent ? 'block' : 'hidden sm:block'}>
+              سایت استخدامی جاب ویژن از سال 1395 فعالیت خود را آغاز کرد. در سال‌های فعالیت خود پیوسته تلاش کرده است با ارائه امکانات و رفع نیازهای شما، مسیر سخت جستجوی کار و کاریابی را به راهی هموار تبدیل کند. در این سایت درج آگهی از سوی کارفرمایان یا مسئولان منابع انسانی انجام شده و نیازمندی های شغلی، حقوق، بیمه و شرایط جذب نیرو در آگهی کار درج می‌شود.
 
-            سایت استخدامی جاب ویژن از سال 1395 فعالیت خود را آغاز کرد. در سال‌های فعالیت خود پیوسته تلاش کرده است با ارائه امکانات و رفع نیازهای شما، مسیر سخت جستجوی کار و کاریابی را به راهی هموار تبدیل کند. در این سایت درج آگهی از سوی کارفرمایان یا مسئولان منابع انسانی انجام شده و نیازمندی های شغلی، حقوق، بیمه و شرایط جذب نیرو در آگهی کار درج می‌شود.
-
-            رزومه ساز دوزبانه رایگان، ارائه آگهی‌های استخدام جدید معتبرترین سازمان‌ها و شرکت های کشور، سیستم هوشمند انطباق، امکان شرکت در آزمون‌های شخصیت شناسی معتبر، به روز بودن فرصت‌های شغلی، ارائه دوره‌های آموزشی برای افزایش مهارت کارجویان و ... از جمله این امکانات است.
+              رزومه ساز دوزبانه رایگان، ارائه آگهی‌های استخدام جدید معتبرترین سازمان‌ها و شرکت های کشور، سیستم هوشمند انطباق، امکان شرکت در آزمون‌های شخصیت شناسی معتبر، به روز بودن فرصت‌های شغلی، ارائه دوره‌های آموزشی برای افزایش مهارت کارجویان و ... از جمله این امکانات است.
+            </span>
           </p>
           <div
             className={`overflow-hidden`}
             ref={footerOtherContentRef}
           >
-            <Title
-              customClass={`mt-6`}
-              withOutIcon
-            >
-              <h6 className={`text-white`}>
-                مراحل و نحوه‌ی استفاده از امکانات جاب ویژن
-              </h6>
-            </Title>
+            <FooterTitle margin>
+              مراحل استفاده از امکانات جاب ویژن
+            </FooterTitle>
             <p className={`text-white text-justify mt-3`}>
               در ادامه، به طور مختصر مراحل و نحوه استفاده از امکاناتی را که سایت کاریابی و استخدامی جاب ویژن در اختیار شما قرار می‌دهد بررسی می‌کنیم.
             </p>
-            <Title
-              customClass={`mt-6`}
-              withOutIcon
-            >
-              <h6 className={`text-white`}>
-                ثبت‌نام در سایت جاب ویژن
-              </h6>
-            </Title>
+            <FooterTitle margin>
+              ثبت‌نام در سایت جاب ویژن
+            </FooterTitle>
             <p className={`text-white text-justify mt-3`}>
               برای ارسال رزومه از طریق سایت استخدامی جاب ویژن، ابتدا باید در سایت ثبت نام کنید. برای این کار روی دکمه‌ی ورود/عضویت کلیک کنید. در این جا، کافی است برای ثبت نام، ایمیل خود را وارد کرده و برای حساب کاربری‌تان یک رمز عبور انتخاب نمایید. علاوه بر آن، امکان ثبت‌نام با گوگل و لینکدین نیز به شما داده شده است.
             </p>
-            <Title withOutIcon>
-              <span className={`text-white mt-6 text-xl`}>
-                لینک های مرتبط
-              </span>
-            </Title>
-            <div className={`flex items-center mt-3`}>
+            <FooterTitle margin>
+              لینک های مرتبط
+            </FooterTitle>
+            <div className={`flex flex-col items-center gap-3 mt-3 sm:flex-row`}>
               <Link
                 to={``}
-                className={`btn decoration-[#ffffff45] text-white underline`}
+                className={`btn btn-dark brightness-110 hover:brightness-125 decoration-[#ffffff35] underline`}
               >
                 استخدام بیمه
               </Link>
               <Link
                 to={``}
-                className={`btn decoration-[#ffffff45] text-white underline`}
+                className={`btn btn-dark brightness-110 hover:brightness-125 decoration-[#ffffff35] underline`}
               >
                 استخدام کارگزاری
               </Link>
               <Link
                 to={``}
-                className={`btn decoration-[#ffffff45] text-white underline`}
+                className={`btn btn-dark brightness-110 hover:brightness-125 decoration-[#ffffff35] underline`}
               >
                 استخدام خودرو
               </Link>
             </div>
-            <Title withOutIcon>
-              <span className={`text-white mt-6 text-xl`}>
-                آخرین مطالب بلاگ
-              </span>
-            </Title>
-            <div className={`flex items-center gap-3 flex-wrap mt-3`}>
+            <FooterTitle margin>
+              آخرین مطالب بلاگ
+            </FooterTitle>
+            <div className={`flex flex-col items-center gap-3 mt-3 sm:flex-row`}>
               <article>
                 <Link
                   to={``}
