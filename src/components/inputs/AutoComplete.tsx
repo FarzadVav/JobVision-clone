fbbf6145cb6c5a15a3ee6104fa3dc30e2bfa4999
FarzadVav <vav.progg.ir@gmail.com>
2023-09-23 07:32:38 -0700

    complete AutoComplete component

--- src/components/inputs/AutoComplete.tsx
+++ src/components/inputs/AutoComplete.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, useState } from "react";
 import tokenGenerator from "../../utils/tokenGenerator";
+import { CloseRounded } from "@mui/icons-material";
 
 type AutoCompleteProps = {
   customClass?: string;
@@ -14,7 +15,15 @@ type AutoCompleteProps = {
 const AutoComplete = ({ customClass, register, setValue, placeholder, error, children, datas }: AutoCompleteProps) => {
   const [focus, setFocus] = useState<boolean>(false)
   const [searchedDatas, setSearchedDatas] = useState<{ name: string; }[]>(datas)
-  const [showDataList, setShowDataList] = useState<boolean>(false)
+
+  const searchedDatasHandler = (value: string) => {
+    setSearchedDatas([])
+    datas.map(data => {
+      if (data.name.includes(value)) {
+        setSearchedDatas(prev => ([...prev, data]))
+      }
+    })
+  }
 
   return (
     <div className={`input-bg group ${error ? 'border-jv-danger hover:border-jv-danger' : (focus && !error) ? 'border-jv-primary' : ''} ${focus ? 'rounded-b-none !border-b-transparent' : ''} ${customClass}`}>
@@ -23,22 +32,24 @@ const AutoComplete = ({ customClass, register, setValue, placeholder, error, chi
         type="text"
         {...register}
         placeholder={placeholder}
-        onChange={(event) => {
-          setSearchedDatas([])
-          datas.map(data => {
-            if (data.name.includes(event.target.value)) {
-              setSearchedDatas(prev => ([...prev, data]))
-            }
-          })
-        }}
-        onFocus={() => {
+        autoComplete="off"
+        onChange={(event) => searchedDatasHandler(event.target.value)}
+        onFocus={(event) => {
+          searchedDatasHandler(event.target.value)
           setFocus(true)
-          setShowDataList(true)
         }}
-        onBlur={() => !showDataList && setFocus(false)}
+        onBlur={() => setFocus(false)}
       />
       <div className={`input-icon ${error ? 'text-jv-danger group-hover:text-jv-danger' : (focus && !error) ? 'text-jv-primary' : 'group-hover:text-jv-primary'}`}>
-        {children}
+        <div className={`w-6 h-6 relative`}>
+          <svg className={`absolute transition-transform !duration-150 ${focus ? 'opacity-0 -translate-y-5' : ''}`}>
+            {children}
+          </svg>
+          <div onMouseDown={() => setValue(register.name, '')}>
+            <CloseRounded className={`cursor-pointer absolute transition-transform !duration-150
+              ${focus ? '' : 'opacity-0 translate-y-5'}`} />
+          </div>
+        </div>
       </div>
       <div className={`bg-jv-bright ring-1 ring-jv-light w-full flex flex-col items-center p-3 pl-2.5 rounded-b
       absolute top-12 right-0 ${focus ? '-translate-y-[1px]' : 'opacity-0 invisible translate-y-6'} z-10`}>
@@ -51,7 +62,7 @@ const AutoComplete = ({ customClass, register, setValue, placeholder, error, chi
                 key={tokenGenerator()}
                 className={`bg-jv-bright border-b border-solid border-jv-light w-full py-2 pr-2 cursor-pointer
                 hover:brightness-[0.97] last:border-none`}
-                onClick={() => {
+                onMouseDown={() => {
                   setValue(register.name, data.name)
                   setFocus(false)
                 }}
