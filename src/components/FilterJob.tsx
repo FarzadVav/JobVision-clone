c2334b72dcc3fb20e9e084379b0d960df1be2643
FarzadVav <vav.progg.ir@gmail.com>
2023-09-28 06:26:44 -0700

    handle mulitFilter in JobsFiltersBar

--- src/components/FilterJob.tsx
+++ src/components/FilterJob.tsx
@@ -33,17 +33,92 @@ const FilterJob = ({ title, clickHandler }: FilterJobProps) => {
   )
 }
 
+type MultiFilterJobProps = {
+  title: string;
+  filters: {
+    title: string;
+    clickHandler: () => void
+  }[];
+}
+
+const MultiFilterJob = ({ title, filters }: MultiFilterJobProps) => {
+  const [select, setSelect] = useState<boolean>(false)
+  const [showMultiSelect, setShowMultiSelect] = useState<boolean>(false)
+
+  return (
+    <button
+      className={`border border-solid flex justify-center items-center px-4 py-1 ml-3 rounded-full
+      cursor-pointer !transition-colors last:ml-0 ${select ? 'bg-jv-primary text-white border-jv-primary pl-1.5' : 'bg-white border-jv-light hover:text-jv-primary'} relative`}
+      onClick={() => {
+        setShowMultiSelect(prev => !prev)
+      }}
+    >
+      {title}
+      {
+        select && (
+          <CloseRounded
+            className='bg-jv-primary text-white brightness-125 mr-3 rounded-full p-0.5'
+            fontSize='small'
+          />
+        )
+      }
+      {
+        showMultiSelect && filters.length ? (
+          <ul className={`light-shadow bg-jv-light w-full flex flex-col absolute top-full right-0 z-10 
+          py-1.5 rounded-md`}>
+            {
+              filters.map(filter => (
+                <li
+                  className={`text-jv-dark w-full px-3 py-1.5`}
+                  onClick={() => {
+                    setSelect(prev => !prev)
+                    filter.clickHandler()
+                  }}
+                >
+                  {filter.title}
+                </li>
+              ))
+            }
+          </ul>
+        ) : null
+      }
+    </button>
+  )
+}
+
 const JobsFiltersBar = () => {
   const filtering = useContext(FilteringContext)
 
   return (
-    <div className={`list-scrollbar w-full mt-6 flex items-center overflow-x-auto pb-3`}>
+    <div className={`list-scrollbar w-full mt-6 flex items-center pb-3`}>
       <FilterJob
         title='دورکرای'
         clickHandler={() => {
-          console.log(123);
-          filtering.setRemote((prev: boolean) => !prev)
+          filtering.setRemoteHandler()
         }}
+      />
+      <FilterJob
+        title='امریه سربازی'
+        clickHandler={() => {
+          filtering.setKnowledgeBasedCompanyHandler()
+        }}
+      />
+      <MultiFilterJob
+        title='نوع همکاری'
+        filters={[
+          {
+            title: 'تمام وقت',
+            clickHandler: () => filtering.setCooperationTypeHandler('full-time')
+          },
+          {
+            title: 'پاره وقت',
+            clickHandler: () => filtering.setCooperationTypeHandler('part-time')
+          },
+          {
+            title: 'پروژه ای',
+            clickHandler: () => filtering.setCooperationTypeHandler('as-projects')
+          }
+        ]}
       />
     </div>
   )
--- src/context/FilteringContext.tsx
+++ src/context/FilteringContext.tsx
@@ -1,53 +1,59 @@
 import { ReactNode, createContext, useState } from "react";
 
-import filtersTypes from "../types/filters.types";
-
-interface FilteringContextProps extends filtersTypes {
-  setRemote: Function;
-  setKnowledgeBasedCompany: Function;
-  setCooperationType: Function;
-  setSalary: Function;
+type filtersTypes = {
+  remote: boolean;
+  setRemoteHandler: (state?: boolean) => void;
+  knowledgeBasedCompany: boolean;
+  setKnowledgeBasedCompanyHandler: (state?: boolean) => void;
+  cooperationType: 'full-time' | 'part-time' | 'as-projects';
+  setCooperationTypeHandler: (type: 'full-time' | 'part-time' | 'as-projects') => void
+  salary: [number, number]
+  setSalaryHandler: (newSalary: [number, number]) => void
 }
 
-const FilteringContext = createContext({} as FilteringContextProps)
+const FilteringContext = createContext({} as filtersTypes)
 
 export const FilteringContextProvider = ({ children }: { children: ReactNode }) => {
   const [remote, setRemote] = useState<boolean>(false)
   const [knowledgeBasedCompany, setKnowledgeBasedCompany] = useState<boolean>(false)
-  const [cooperationType, setCooperationType] = useState<{
-    fullTime: boolean;
-    partTime: boolean;
-    asProject: boolean;
-  }>({
-    fullTime: false,
-    partTime: false,
-    asProject: false
-  })
-  const [salary, setSalary] = useState<{
-    one: [0, 4];
-    two: [4, 8];
-    three: [8, 15];
-    four: [15, 25];
-    five: [25, 75]
-  }>({
-    one: [0, 4],
-    two: [4, 8],
-    three: [8, 15],
-    four: [15, 25],
-    five: [25, 75]
-  })
+  const [cooperationType, setCooperationType] = useState<'full-time' | 'part-time' | 'as-projects'>('full-time')
+  const [salary, setSalary] = useState<[number, number]>([0, 75])
+
+  const setRemoteHandler = (state?: boolean) => {
+    if (typeof state === 'undefined') {
+      setRemote(prev => !prev)
+    } else {
+      setRemote(state)
+    }
+  }
+
+  const setKnowledgeBasedCompanyHandler = (state?: boolean) => {
+    if (typeof state === 'undefined') {
+      setKnowledgeBasedCompany(prev => !prev)
+    } else {
+      setKnowledgeBasedCompany(state)
+    }
+  }
+
+  const setCooperationTypeHandler = (type: 'full-time' | 'part-time' | 'as-projects') => {
+    setCooperationType(type)
+  }
+
+  const setSalaryHandler = (newSalary: [number, number]) => {
+    setSalary(newSalary)
+  }
 
   return (
     <FilteringContext.Provider
       value={{
         remote,
-        setRemote,
+        setRemoteHandler,
         knowledgeBasedCompany,
-        setKnowledgeBasedCompany,
+        setKnowledgeBasedCompanyHandler,
         cooperationType,
-        setCooperationType,
+        setCooperationTypeHandler,
         salary,
-        setSalary
+        setSalaryHandler
       }}
     >
       {children}
