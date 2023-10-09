ffd5817ec9a31ee1603b794469e19708430d0e1c
FarzadVav <vav.progg.ir@gmail.com>
2023-10-09 03:24:22 -0700

    add jobAds apply request in dashboard

--- src/pages/dashborad/Employer.tsx
+++ src/pages/dashborad/Employer.tsx
@@ -5,9 +5,9 @@ const Employer = () => {
   return (
     <div className={`wrapper flex my-12`}>
       <aside className={`bg-jv-primary w-3/12 h-max flex flex-col justify-center items-center p-5 rounded-md sticky top-[5.25rem] overflow-hidden`}>
-        <div className={`bg-jv-bright w-20 h-20 flex justify-center items-center rounded-full relative`}>
+        <div className={`w-20 h-20 flex justify-center items-center rounded-full relative`}>
           <img
-            className={`w-32 h-32 object-cover object-center rounded-full absolute blur-2xl opacity-60`}
+            className={`w-32 h-32 object-cover object-center rounded-full absolute blur-2xl opacity-50`}
             src="https://fileapi.jobvision.ir/api/v1.0/files/getimage?fileid=3518088&width=70&height=70"
             alt="لوگوی شرکت"
           />
--- src/pages/dashborad/EmployerDashboard.tsx
+++ src/pages/dashborad/EmployerDashboard.tsx
@@ -1,6 +1,71 @@
+import { AccountCircle, DoNotDisturbAltRounded, DoneRounded } from "@mui/icons-material"
+
 const EmployerDashboard = () => {
   return (
-    <div>EmployerDashboard</div>
+    <div className={`w-full flex flex-col`}>
+      <ul className={`w-full flex flex-col justify-center items-center`}>
+        <li className={`bg-jv-light w-full flex justify-between items-center p-2 pr-3 rounded`}>
+          <div className={`flex items-center`}>
+            <AccountCircle
+              className={`text-jv-primary`}
+              fontSize="large"
+            />
+            <span className={`dana-bold mr-3`}>
+              فرزاد وحدتی نژاد
+            </span>
+          </div>
+          <div className={`flex items-center`}>
+            <button className={`btn btn-success`}>
+              استخدام
+              <DoneRounded />
+            </button>
+            <button className={`btn btn-danger mr-2`}>
+              <DoNotDisturbAltRounded />
+            </button>
+          </div>
+        </li>
+        <li className={`bg-jv-light w-full flex justify-between items-center p-2 pr-3 rounded mt-2`}>
+          <div className={`flex items-center`}>
+            <AccountCircle
+              className={`text-jv-primary`}
+              fontSize="large"
+            />
+            <span className={`dana-bold mr-3`}>
+              فرزاد وحدتی نژاد
+            </span>
+          </div>
+          <div className={`flex items-center`}>
+            <button className={`btn btn-success`}>
+              استخدام
+              <DoneRounded />
+            </button>
+            <button className={`btn btn-danger mr-2`}>
+              <DoNotDisturbAltRounded />
+            </button>
+          </div>
+        </li>
+        <li className={`bg-jv-light w-full flex justify-between items-center p-2 pr-3 rounded mt-2`}>
+          <div className={`flex items-center`}>
+            <AccountCircle
+              className={`text-jv-primary`}
+              fontSize="large"
+            />
+            <span className={`dana-bold mr-3`}>
+              فرزاد وحدتی نژاد
+            </span>
+          </div>
+          <div className={`flex items-center`}>
+            <button className={`btn btn-success`}>
+              استخدام
+              <DoneRounded />
+            </button>
+            <button className={`btn btn-danger mr-2`}>
+              <DoNotDisturbAltRounded />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
 
