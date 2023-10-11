ff432ced747c65540af31771161d2e860ccbc7e3
FarzadVav <vav.progg.ir@gmail.com>
2023-10-11 08:31:22 -0700

    change company profile in /d_employer

--- src/pages/dashborad/Employer.tsx
+++ src/pages/dashborad/Employer.tsx
@@ -5,15 +5,15 @@ const Employer = () => {
   return (
     <div className={`wrapper flex my-12`}>
       <aside className={`bg-jv-primary w-3/12 h-max flex flex-col justify-center items-center p-5 rounded-md sticky top-[5.25rem] overflow-hidden`}>
-        <div className={`w-20 h-20 flex justify-center items-center rounded-full relative`}>
+        <div className={`bg-white w-20 h-20 flex justify-center items-center rounded-md relative`}>
           <img
-            className={`w-32 h-32 object-cover object-center rounded-full absolute blur-2xl opacity-50`}
-            src="https://fileapi.jobvision.ir/api/v1.0/files/getimage?fileid=3518088&width=70&height=70"
+            className={`w-32 h-32 object-cover object-center rounded-md absolute blur-2xl opacity-50`}
+            src="https://fileapi.jobvision.ir/api/v1.0/files/getimage?fileid=151196&width=70&height=70"
             alt="لوگوی شرکت"
           />
           <img
-            className={`w-full h-full object-cover object-center rounded-full absolute`}
-            src="https://fileapi.jobvision.ir/api/v1.0/files/getimage?fileid=3518088&width=70&height=70"
+            className={`w-full h-full object-cover object-center rounded-md absolute`}
+            src="https://fileapi.jobvision.ir/api/v1.0/files/getimage?fileid=151196&width=70&height=70"
             alt="لوگوی شرکت"
           />
         </div>
