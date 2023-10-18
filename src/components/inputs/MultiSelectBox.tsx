967af17977f632d9ba4e6cabce8b1d726e7973db
FarzadVav <vav.progg.ir@gmail.com>
2023-10-18 07:58:33 -0700

    change MultiSelect stracture

--- src/components/inputs/MultiSelectBox.tsx
+++ src/components/inputs/MultiSelectBox.tsx
@@ -37,74 +37,96 @@ const MultiSelectBox = ({
   }
 
   return (
-    <div
-      className={`input-bg group ${error ? 'border-jv-danger hover:border-jv-danger' : (focus && !error) ? 'border-jv-primary' : ''} ${focus ? 'rounded-b-none !border-b-transparent' : ''} ${customClass}`}
-      onClick={event => {
-        inputRef.current?.focus()
-        setFocus(true)
-        event.stopPropagation()
-      }}
-    >
-      <input
-        className={`input`}
-        type="text"
-        ref={inputRef}
-        value={value}
-        placeholder={placeholder}
-        autoComplete="off"
-        onChange={(event) => {
-          setValue(event.target.value)
-          searchedDatasHandler(event.target.value)
+    <div className={`w-full flex flex-col`}>
+      <div
+        className={`input-bg group ${error ? 'border-jv-danger hover:border-jv-danger' : (focus && !error) ? 'border-jv-primary' : ''} ${focus ? 'rounded-b-none !border-b-transparent' : ''} ${customClass}`}
+        onClick={event => {
+          inputRef.current?.focus()
+          setFocus(true)
+          event.stopPropagation()
         }}
-        onFocus={event => searchedDatasHandler(event.target.value)}
-      />
-      <div className={`input-icon ${error ? 'text-jv-danger group-hover:text-jv-danger' : (focus && !error) ? 'text-jv-primary' : 'group-hover:text-jv-primary'}`}>
-        <div className={`w-6 h-6 relative`}>
-          <svg className={`absolute transition-transform !duration-150 ${focus ? 'opacity-0 -translate-y-5 invisible' : ''}`}>
-            <KeyboardArrowDownRounded />
-          </svg>
-          <div
-            onClick={event => {
-              event.stopPropagation()
-              setFocus(false)
-              setValue('')
-              resetHandler()
-            }}
-          >
-            <CloseRounded className={`cursor-pointer absolute transition-transform !duration-150 hover:text-jv-danger
+      >
+        <input
+          className={`input`}
+          type="text"
+          ref={inputRef}
+          value={value}
+          placeholder={placeholder}
+          autoComplete="off"
+          onChange={(event) => {
+            setValue(event.target.value)
+            searchedDatasHandler(event.target.value)
+          }}
+          onFocus={event => searchedDatasHandler(event.target.value)}
+        />
+        <div className={`input-icon ${error ? 'text-jv-danger group-hover:text-jv-danger' : (focus && !error) ? 'text-jv-primary' : 'group-hover:text-jv-primary'}`}>
+          <div className={`w-6 h-6 relative`}>
+            <svg className={`absolute transition-transform !duration-150 ${focus ? 'opacity-0 -translate-y-5 invisible' : ''}`}>
+              <KeyboardArrowDownRounded />
+            </svg>
+            <div
+              onClick={event => {
+                event.stopPropagation()
+                setFocus(false)
+              }}
+            >
+              <CloseRounded className={`cursor-pointer absolute transition-transform !duration-150 hover:text-jv-danger
               ${focus ? '' : 'opacity-0 translate-y-5 invisible'}`} />
+            </div>
           </div>
         </div>
+        <div className={`bg-jv-bright ring-1 ring-jv-light w-full flex flex-col items-center p-3 pl-2.5 rounded-b
+          absolute top-12 right-0 ${focus ? '-translate-y-[1px]' : 'opacity-0 invisible translate-y-6'} z-10`}>
+          <div className={`bg-jv-bright border-t border-l border-solid border-jv-light w-3 h-3 rotate-45
+            absolute -top-1.5 z-10`}></div>
+          <ul className={`list-scrollbar w-full max-h-72 pl-3 overflow-y-auto z-20`}>
+            {
+              list.length ? (
+                <li className={`w-full flex items-center justify-end`}>
+                  <div
+                    className={`btn-sm btn-danger`}
+                    onClick={() => {
+                      setValue('')
+                      resetHandler()
+                    }}
+                  >
+                    پاک کردن
+                  </div>
+                </li>
+              ) : null
+            }
+            {
+              searchedDatas.length ? searchedDatas.map(data => (
+                <li
+                  key={tokenGenerator()}
+                  className={`bg-jv-bright border-b border-solid border-jv-light w-full py-2 pr-2 cursor-pointer flex items-center group/verify hover:brightness-[0.97] last:border-none`}
+                  onMouseDown={() => {
+                    if (list.includes(data)) {
+                      unSelectHandler(data)
+                    } else {
+                      addItemHandler(data)
+                    }
+                    setValue('')
+                  }}
+                >
+                  <DoneRounded
+                    className={`text-jv-primary ${list.includes(data) ? '' : '!hidden'} ml-3  group-hover/verify:text-jv-danger`}
+                    fontSize="small"
+                  />
+                  {data}
+                </li>
+              )) : 'موردی برای نشان دادن وجود ندارد'
+            }
+          </ul>
+        </div>
       </div>
-      <div className={`bg-jv-bright ring-1 ring-jv-light w-full flex flex-col items-center p-3 pl-2.5 rounded-b
-      absolute top-12 right-0 ${focus ? '-translate-y-[1px]' : 'opacity-0 invisible translate-y-6'} z-10`}>
-        <div className={`bg-jv-bright border-t border-l border-solid border-jv-light w-3 h-3 rotate-45
-        absolute -top-1.5 z-10`}></div>
-        <ul className={`list-scrollbar w-full max-h-72 pl-3 overflow-y-auto z-20`}>
-          {
-            searchedDatas.length ? searchedDatas.map(data => (
-              <li
-                key={tokenGenerator()}
-                className={`bg-jv-bright border-b border-solid border-jv-light w-full py-2 pr-2 cursor-pointer flex items-center group/verify hover:brightness-[0.97] last:border-none`}
-                onMouseDown={() => {
-                  if (list.includes(data)) {
-                    unSelectHandler(data)
-                  } else {
-                    addItemHandler(data)
-                  }
-                  setValue('')
-                }}
-              >
-                <DoneRounded
-                  className={`text-jv-primary ${list.includes(data) ? '' : '!hidden'} ml-3  group-hover/verify:text-jv-danger`}
-                  fontSize="small"
-                />
-                {data}
-              </li>
-            )) : 'موردی برای نشان دادن وجود ندارد'
-          }
-        </ul>
-      </div>
+      {
+        list.length ? (
+          <span className={`text-sm mt-3`}>
+            {list.length} آیتم اضافه نشده
+          </span>
+        ) : null
+      }
     </div>
   )
 }
