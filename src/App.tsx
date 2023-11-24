94ec2af4dd4d49356acd5c9bcfa537620b6a14f2
FarzadVav <vav.progg.ir@gmail.com>
2023-11-24 04:52:50 -0800

    add supabase to app

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,21 @@ import routes from "./routes.tsx";
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
 import { AuthContextProvider } from "./context/AuthContext.tsx";
+import { useEffect } from "react";
+import supabase from "./utils/supabase.ts";
 
 const App = () => {
 	const router = useRoutes(routes)
+
+	useEffect(() => {
+		const func = async () => {
+			const { data: jobAds } = await supabase
+				.from('jobAds')
+				.select('*')
+			console.log(jobAds);
+		}
+		func()
+	}, [])
 
 	return (
 		<AuthContextProvider>
