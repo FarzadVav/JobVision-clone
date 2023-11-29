835f5b8405ae3cd405128fb92814cf28fd9b6a71
FarzadVav <vav.progg.ir@gmail.com>
2023-11-28 22:17:05 -0800

    complete useDetailsJobAds

--- src/store/useDetailsJobAds.ts
+++ src/store/useDetailsJobAds.ts
@@ -3,19 +3,33 @@ import supabase from "../utils/supabase";
 import CooperationTypes from "../types/cooperationTypes.type";
 
 type useDetailsJobAdsTypes = {
-  cooperationType: { id: string; name: CooperationTypes }[];
+  cooperationType: { _id: string; name: CooperationTypes }[];
+  provinces: { _id: string; name: CooperationTypes }[];
+  cities: { _id: string; name: CooperationTypes }[];
   getDetails: () => void
 }
 
 const useDetailsJobAds = create<useDetailsJobAdsTypes>(set => ({
   cooperationType: [],
+  provinces: [],
+  cities: [],
   getDetails: async () => {
     const { data: cooperationTypes } = await supabase
       .from('cooperationTypes')
       .select('*')
 
+    const { data: provinces } = await supabase
+      .from('provinces')
+      .select('*')
+
+    const { data: cities } = await supabase
+      .from('cities')
+      .select('*')
+
     set({
-      cooperationType: cooperationTypes || []
+      cooperationType: cooperationTypes || [],
+      provinces: provinces || [],
+      cities: cities || []
     })
   }
 }))
