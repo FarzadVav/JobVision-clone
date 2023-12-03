78eec100082f15a9fb64127d11ee46716bb82664
FarzadVav <vav.progg.ir@gmail.com>
2023-12-03 06:31:29 -0800

    fixed ReactQueryDevtools error and config react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,25 @@
 import { useRoutes } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import routes from "./routes.tsx";
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
-import { AuthContextProvider } from "./context/AuthContext.tsx";
 import useLoading from "./store/useLoading.ts";
-
-const client = new QueryClient()
+import { AuthContextProvider } from "./context/AuthContext.tsx";
 
 const App = () => {
 	const router = useRoutes(routes)
 	const { appLoading, pageLoading } = useLoading(s => s)
 
 	return (
-		<QueryClientProvider client={client}>
-			<AuthContextProvider>
-				<Header />
-				{router}
-				{
-					!location.pathname.includes('employer') ? <Footer /> : null
-				}
+		<AuthContextProvider>
+			<Header />
+			{router}
+			{
+				!location.pathname.includes('employer') ? <Footer /> : null
+			}
 
-				<div className={`bg-jv-primary h-0.5 fixed top-[4.5rem] left-0 z-50 ${(appLoading || pageLoading) ? 'loading-bar' : 'duration-1000 w-full opacity-0'}`}></div>
-			</AuthContextProvider>
-			<ReactQueryDevtools />
-		</QueryClientProvider>
+			<div className={`bg-jv-primary h-0.5 fixed top-[4.5rem] left-0 z-50 ${(appLoading || pageLoading) ? 'loading-bar' : 'duration-1000 w-full opacity-0'}`}></div>
+		</AuthContextProvider>
 	);
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,26 @@
-import { lazy } from 'react';
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const App = lazy(() => import('./App.tsx'))
+import App from './App';
 import './main.css'
 
+const client = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 30_000
+		}
+	}
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<BrowserRouter>
-			<App />
-	</BrowserRouter>
+	<>
+		<BrowserRouter>
+			<QueryClientProvider client={client}>
+				<App />
+				<ReactQueryDevtools />
+			</QueryClientProvider >
+		</BrowserRouter>
+	</>
 )
